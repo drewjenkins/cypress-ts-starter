98e7dc5f6986635ef79b72e2b6c9c94250fe3fa0
Andrew Jenkins <ajenkins@hubspot.com>
2021-08-26 11:06:07 -0400

    Initial Commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('hey')
+import cypress from 'cypress';
+
+cypress.run({
+  browser: 'chrome',
+  config: {
+    baseUrl: 'http://www.blank.org',
+    env: {},
+    numTestsKeptInMemory: 1,
+    watchForFileChanges: true
+  }
+});
---
+++ src/sites/google.ts
@@ -0,0 +1,5 @@
+describe('My First Test', () => {
+  it('Visits Google', () => {
+    cy.visit('https://google.com');
+  });
+});
